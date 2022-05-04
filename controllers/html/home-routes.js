89a818a590ee1a747eaed7829e4187a6da6d95e6
ApolloSolo <matt1teixeira@gmail.com>
2022-05-04 09:32:27 -0400

    Able to view recipes by category via navbar dropdown

--- controllers/html/home-routes.js
+++ controllers/html/home-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const getRecipes = require("../../public/js/yum");
+const getRecipes = require("../../public/js/tasty");
 
 router.get("/", (req, res) => {
   res.render("homepages/homepage", { loggedIn: req.session.loggedIn });
@@ -20,7 +20,34 @@ router.get("/dashboard", async (req, res) => {
         instructions: recipeData[i].instructions,
       };
       reducedRecipes.push(newRecipeObj);
-    } 
+    }
+  }
+  console.log(reducedRecipes);
+
+  res.render("homepages/dashboard", {
+    loggedIn: req.session.loggedIn,
+    username: req.session.username,
+    reducedRecipes,
+  });
+});
+
+router.get("/dashboard/:category", async (req, res) => {
+  const { category } = req.params;
+  const recipeData = await getRecipes(category);
+  let reducedRecipes = [];
+  for (let i = 0; i < recipeData.length; i++) {
+    if (!recipeData[i].recipes) {
+      let newRecipeObj = {
+        name: recipeData[i].name,
+        description: recipeData[i].description,
+        img:
+          recipeData[i].thumbnail_url ||
+          "https://images.unsplash.com/photo-1588503812457-f289d7f27314?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZW1wdHklMjBwbGF0ZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        video: recipeData[i].original_video_url,
+        instructions: recipeData[i].instructions,
+      };
+      reducedRecipes.push(newRecipeObj);
+    }
   }
   console.log(reducedRecipes);
 
